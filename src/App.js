fdd15e38b5b7326c40f9e779dd38f3fae0b07496
chetanbavare <chetsmartk@gmail.com>
2023-12-14 00:39:06 +0530

    third commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Tours from "./components/Tours";
+import data from "./data";
+
+const App = () => {
+
+ const [tours, setTours] = useState(data);
+
+  function removeTour(id)
+  {
+    const newTours = tours.filter(tour => tour.id !== id); 
+    setTours(newTours) 
+  }
+
+  if(tours.length === 0)
+  {
+    return(
+      <div className="noTours">
+        <h2 className="tours_heading">No Tours Left</h2>
+        <button onClick={() => setTours(data)}>Refresh</button>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Tours tours = {tours} removeTour={removeTour}></Tours>
+    </div>
+  )
+};
+
+export default App;
